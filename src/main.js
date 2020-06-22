ad981a832d55e234927705be7907242624d2f367
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-22 11:38:07 -0700

    Update ping-pong-2.js with function and export to main.js which holds user interface js.
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { pingPong2 } from './ping-pong-2';
+
+document).ready(function() {
+  $('#ping-pong-2-form').submit(function(event) {
+    event.preventDefault();
+    const goal = $('#goal').val();
+    const output = pingPong2(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
